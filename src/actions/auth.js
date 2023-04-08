09fa96a869cd66dcba3b80077f84e78bd26e33c5
CherokeeX <serefb80@gmail.com>
2023-04-08 20:21:39 +0300

    commit

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class auth extends Component {
   render() {
     return (
-      <div>auth</div>
+      <div> <p>deneme</p></div>
     )
   }
 }
